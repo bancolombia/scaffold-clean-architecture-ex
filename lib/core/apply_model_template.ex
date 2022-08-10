a292b12c36145cbd9e9ba496960dd189269f6b8f
Santiago Calle Gomez <sancalle@bancolombia.com.co>
2022-08-09 23:58:20 -0500

    New feature to create behaviours, it is optional when a new model will created

--- lib/core/apply_model_template.ex
+++ lib/core/apply_model_template.ex
@@ -3,6 +3,7 @@ defmodule ElixirStructureManager.Core.ApplyModelTemplate do
   alias ElixirStructureManager.Utils.StringContent
 
   @model_template_path "/priv/create_structure/templates/model.txt"
+  @behaviour_template_path "/priv/create_structure/templates/behaviour.txt"
 
   defp format_name(name) do
     case String.match?(name, ~r/^([a-zA-Z0-9]+_[a-zA-Z0-9]+){1,}$/) do
@@ -22,25 +23,44 @@ defmodule ElixirStructureManager.Core.ApplyModelTemplate do
   def create_model(app_name, model_name) do
     with {:ok, _app_snake_name, app_camel_name} <- format_name(app_name),
          {:ok, model_snake_name, model_camel_name} <- format_name(model_name) do
+      
       project_model_path = "lib/domain/model/" <> model_snake_name <> ".ex"
-
-      create_file(app_camel_name, model_camel_name, project_model_path)
+      
+      token_list = [
+        %{name: "{module_name}", value: app_camel_name},
+        %{name: "{model_name}", value: model_camel_name}
+      ]
+      
+      create_file(project_model_path, @model_template_path, token_list)
     else
       err -> Mix.raise("Invalid name indicated: " <> elem(err, 2))
     end
   end
 
-  defp create_file(app_name, model_name, project_model_path) do
+  def create_behaviour(app_name, behaviour_name) do
+    with {:ok, _app_snake_name, app_camel_name} <- format_name(app_name),
+         {:ok, behaviour_snake_name, behaviour_camel_name} <- format_name(behaviour_name) do
+      
+      project_behaviour_path = "lib/domain/behaviours/" <> behaviour_snake_name <> ".ex"
+      
+      token_list = [
+        %{name: "{module_name}", value: app_camel_name},
+        %{name: "{behaviour_name}", value: behaviour_camel_name}
+      ]
+      
+      create_file(project_behaviour_path, @behaviour_template_path, token_list)
+    else
+      err -> Mix.raise("Invalid name indicated: " <> elem(err, 2))
+    end
+  end
+
+  defp create_file(project_file_path, template_path, token_list) do
     app_path = Application.app_dir(:elixir_structure_manager)
 
-    with {:ok, file_template_content} <- File.read(app_path <> @model_template_path) do
-      token_list = [
-        %{name: "{module_name}", value: app_name},
-        %{name: "{model_name}", value: model_name}
-      ]
-
+    with {:ok, file_template_content} <- File.read(app_path <> template_path) do
+      
       file_content = StringContent.replace(token_list, file_template_content)
-      File.write!(project_model_path, file_content)
+      File.write!(project_file_path, file_content)
     else
       err -> IO.inspect(err)
     end

8087105ffdccc6da78eb1838665f2f99288fddb5
Santiago Calle Gomez <sancalle@bancolombia.com.co>
2022-08-24 17:05:59 -0500

    Created new unit tests

--- test/apply_model_test.exs
+++ test/apply_model_test.exs
@@ -26,11 +26,43 @@ defmodule ApplyModelTest do
     end
   end
 
-  test "should fail when file does not exist" do
+  test "should fail when file does not exist creating a model" do
     
     with_mock File, [read: fn(_path) -> {:error, :enoent} end] do
 
       res = ApplyModelTemplate.create_model("test_project", "test_model")
+
+     assert {:error, :enoent} == res 
+    end
+    
+  end
+
+  test "should create a behaviour" do
+    
+    with_mocks([
+
+      {Application, [], [app_dir: fn(:elixir_structure_manager) -> "user/path/scaffold_project/" end]},
+        {File, [], [write!: fn(_path, _content) -> :ok end]},
+      {File, [], [read: fn (_path) -> { :ok, "{application_name}-{behaviour_name}" } end]}
+    ]) do
+
+      res = ApplyModelTemplate.create_behaviour("test_project", "test_behaviour")
+      assert :ok == res
+    end
+     
+  end
+
+  test "should fail when behaviour name is invalid name" do
+    assert_raise Mix.Error, ~r"Invalid name indicated: ", fn -> 
+      ApplyModelTemplate.create_behaviour("test_project", "invalidname")
+    end
+  end
+
+  test "should fail when file does not exist creating a behaviour" do
+    
+    with_mock File, [read: fn(_path) -> {:error, :enoent} end] do
+
+      res = ApplyModelTemplate.create_behaviour("test_project", "test_behaviour")
 
      assert {:error, :enoent} == res 
     end
--- test/data_type_utils_test.exs
+++ test/data_type_utils_test.exs
@@ -1,6 +1,6 @@
 defmodule DataTypeUtilsTest do
   use ExUnit.Case
-  alias ElixirStructureManager.Core.DataTypeUtils
+  alias ElixirStructureManager.Utils.DataTypeUtils
 
   defstruct [
     name: "",
@@ -37,4 +37,33 @@ defmodule DataTypeUtilsTest do
     assert model == res_structure
   end
 
+  test "should return arguments values" do
+
+    switches = [type: :string, name: :string]
+
+    values = DataTypeUtils.parse_opts(
+      ["--type", "secrets", "--name", "test"], switches)  
+    assert values == {[type: "secrets", name: "test"], []}
+    
+    values_without_flag = DataTypeUtils.parse_opts(
+      ["username", "--name", "test"], switches)
+    
+    assert values_without_flag == {[name: "test"], ["username"]}
+  end
+  
+  test "should raise a message when args are invalid" do
+
+    switches = [type: :string, name: :string]
+
+    assert_raise Mix.Error, ~r/Invalid option:\s+/,
+      fn -> DataTypeUtils.parse_opts(["--types", "secrets"], switches) 
+      end
+
+    # assert values == {[type: "secrets", name: "test"], []}
+    
+    #values_without_flag = DataTypeUtils.parse_opts(
+    #  ["username", "--names", "test"], switches)
+    
+    # assert values_without_flag == {[name: "test"], ["username"]}
+  end
 end

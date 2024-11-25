7e4a3e8ae7a2664a776d69301803527671361185
jksalaza <jksalaza@bancolombia.com.co>
2024-11-25 10:42:54 -0500

    fix(tests): increase test coverage

--- priv/templates/structure/health_check_test.exs
+++ priv/templates/structure/health_check_test.exs
@@ -3,17 +3,6 @@ defmodule {app}.Infrastructure.EntryPoints.HealthCheckTest do
 
   use ExUnit.Case
 
-  describe "checks/0" do
-    test "returns a list of health checks" do
-      checks = HealthCheck.checks()
-
-      check = hd(checks)
-      assert check.name == "http"
-      assert check.module == HealthCheck
-      assert check.function == :check_http
-    end
-  end
-
   describe "check_http/0" do
     test "returns :ok" do
       assert HealthCheck.check_http() == :ok
